572cb4aeba77419b5a40d26ab03811510978bb1b
Szymon Sztuka <szymon.sztuka@yahoo.com>
2015-09-13 23:13:41 +0100

    JCraft JSch added, first code to copy a file from remote server

---
+++ src/main/java/serverchainsimulator/ScpFrom.java
@@ -0,0 +1,148 @@
+/* Altered copy of http://www.jcraft.com/jsch/examples/ScpFrom.java.html */
+package serverchainsimulator;
+
+import com.jcraft.jsch.*;
+import java.io.*;
+
+public class ScpFrom{
+
+    public static void get(String user, String host, String password, String localDestFile, String remoteSrcFile ){
+
+        FileOutputStream fos = null;
+        try {
+          JSch jsch = new JSch();
+            Session session=jsch.getSession(user, host, 22);
+
+            // username and password will be given via UserInfo interface.
+            UserInfo ui = new MyUserInfo(password);
+            session.setUserInfo(ui);
+            session.connect();
+
+            // exec 'scp -f rfile' remotely
+            String command = "scp -f "+remoteSrcFile;
+            Channel channel = session.openChannel("exec");
+            ((ChannelExec)channel).setCommand(command);
+
+            // get I/O streams for remote scp
+            OutputStream out = channel.getOutputStream();
+            InputStream in = channel.getInputStream();
+
+            channel.connect();
+
+            byte[] buf = new byte[1024];
+
+            // send '\0'
+            buf[0] = 0; out.write(buf, 0, 1); out.flush();
+
+            while(true){
+                int c=checkAck(in);
+                if(c!='C'){
+                    break;
+                }
+
+                // read '0644 '
+                in.read(buf, 0, 5);
+
+                long filesize=0L;
+                while(true){
+                    if(in.read(buf, 0, 1)<0){
+                        // error
+                        break;
+                    }
+                    if(buf[0]==' ')break;
+                    filesize=filesize*10L+(long)(buf[0]-'0');
+                }
+
+                String file;
+                for(int i=0;;i++){
+                    in.read(buf, i, 1);
+                    if(buf[i]==(byte)0x0a){
+                        file=new String(buf, 0, i);
+                        break;
+                    }
+                }
+                System.out.println("filesize="+filesize+", file="+file);
+
+                // send '\0'
+                buf[0]=0; out.write(buf, 0, 1); out.flush();
+
+                // read a content of lfile
+                fos=new FileOutputStream( localDestFile);
+                int foo;
+                while(true){
+                    if(buf.length<filesize) foo=buf.length;
+                    else foo=(int)filesize;
+                    foo=in.read(buf, 0, foo);
+                    if(foo<0){
+                        // error
+                        break;
+                    }
+                    fos.write(buf, 0, foo);
+                    filesize-=foo;
+                    if(filesize==0L) break;
+                }
+                fos.close();
+                fos=null;
+
+                if(checkAck(in)!=0){
+                    return;
+                }
+
+                // send '\0'
+                buf[0]=0; out.write(buf, 0, 1); out.flush();
+            }
+
+            session.disconnect();
+        }
+        catch(Exception e){
+            System.out.println(e);
+            try{if(fos!=null)fos.close();}catch(Exception ee){}
+        }
+    }
+
+    static int checkAck(InputStream in) throws IOException{
+        int b=in.read();
+        // b may be 0 for success,
+        //          1 for error,
+        //          2 for fatal error,
+        //          -1
+        if(b==0) return b;
+        if(b==-1) return b;
+
+        if(b==1 || b==2){
+            StringBuffer sb=new StringBuffer();
+            int c;
+            do {
+                c=in.read();
+                sb.append((char)c);
+            }
+            while(c!='\n');
+            if(b==1){ // error
+                System.out.print(sb.toString());
+            }
+            if(b==2){ // fatal error
+                System.out.print(sb.toString());
+            }
+        }
+        return b;
+    }
+
+    public static class MyUserInfo implements UserInfo{
+        String passwd;
+        public MyUserInfo (String password) {
+            this.passwd = password;
+        }
+        public String getPassword(){ return passwd; }
+        @Override
+        public boolean promptYesNo(String str){   return true;    }
+        @Override
+        public String getPassphrase(){ return null; }
+        @Override
+        public boolean promptPassphrase(String message){ return true; }
+        @Override
+        public boolean promptPassword(String message){     return true;      }
+        @Override
+        public void showMessage(String message){
+        }
+    }
+}
