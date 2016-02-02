689b3e0258fe850090636041e79b834d831c513c
szymonsztuka <szymon.sztuka@interactivedata.com>
2016-02-02 17:34:27 +0000

    refactoring, modified logs, removed obsolete code

---
+++ src/main/java/serverchainsimulator/content/SimpleMessageGenerator.java
@@ -0,0 +1,11 @@
+package serverchainsimulator.content;
+
+import java.nio.ByteBuffer;
+
+public class SimpleMessageGenerator implements MessageGenerator {
+    public boolean write(final String input, final ByteBuffer output, boolean sendAtTimestamps) {
+        byte[] body = input.getBytes();
+        output.put(body);
+        return true;
+    }
+}
