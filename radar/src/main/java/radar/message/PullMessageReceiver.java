c12b414815d2275542305d2b9415b45d721ef010
szymonsztuka <szymon.sztuka@interactivedata.com>
2016-04-01 16:51:26 +0100

    build.gradle multiproject for jitpack

--- radar/src/main/java/radar/message/PullMessageReceiver.java
+++
@@ -1,9 +0,0 @@
-package radar.message;
-
-import java.io.ByteArrayOutputStream;
-import java.nio.ByteBuffer;
-
-public interface PullMessageReceiver {
-	 boolean read(ByteBuffer	bis, boolean firstMessage, ByteArrayOutputStream baos);
-	 String get(ByteArrayOutputStream baos) ;
-}
