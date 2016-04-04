b622ca2edafccd3dc4c0cd93b3892208b3bf86f4
szymonsztuka <szymon.sztuka@interactivedata.com>
2016-04-04 22:20:57 +0100

    dependency injection of node factory and message factories

---
+++ radar-samples/src/main/java/radar/message/DummyScriptFactory.java
@@ -0,0 +1,9 @@
+package radar.message;
+
+public class DummyScriptFactory implements ScriptFactory {
+
+    @Override
+    public Script getScriptGenerator(String... args) {
+        return new DummyScript();
+    }
+}
